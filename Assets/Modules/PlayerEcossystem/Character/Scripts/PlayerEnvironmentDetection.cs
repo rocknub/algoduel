7aa7fb48ef4e44154fe03829f0abacfd883e50ce
Marlus Vinicius <marlus.cabral@compasso.com.br>
2024-04-04 00:52:10 -0300

    (chore) implement visual debug for obstacles and fall

--- Assets/Modules/PlayerEcossystem/Character/Scripts/PlayerEnvironmentDetection.cs
+++ Assets/Modules/PlayerEcossystem/Character/Scripts/PlayerEnvironmentDetection.cs
@@ -1,9 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Character
 {
     public class PlayerEnvironmentDetection : MonoBehaviour
     {
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField] private LayerMask obstacleMask;
+        [Header("Debug")] 
+        [SerializeField] private bool showGizmos = true;
+        [SerializeField] private float obstacleCheckDebugDistance;
+
+        public bool IsAboveGround()
+        {
+            return Physics.Raycast(transform.position, -transform.up, 100, groundMask);
+        }
+
+        public bool CheckForObstacles(float distance)
+        {
+            return Physics.Raycast(transform.position, transform.forward, distance, obstacleMask);
+        }
         
+        private void OnDrawGizmosSelected()
+        {
+            if (showGizmos == false)
+                return;
+            Gizmos.color = IsAboveGround() ? Color.blue : Color.red;
+            Gizmos.DrawLine(transform.position, transform.position - transform.up * 100f);
+            Gizmos.color = CheckForObstacles(obstacleCheckDebugDistance) ? Color.red : Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * obstacleCheckDebugDistance);
+        }
     }
 }
