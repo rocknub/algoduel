83baabcf4c4357bcb080c95dfea6091b6977e211
Marlus Vinicius <marlus.cabral@compasso.com.br>
2024-04-18 22:12:10 -0300

    (chore) insert fire icon

---
+++ Assets/Modules/PlayerEcossystem/Character/Scripts/PlayerFire.cs
@@ -0,0 +1,17 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Character
+{
+    public class PlayerFire : SingleTaskComponent
+    {
+        [SerializeField] private Transform fireOrigin;
+        [SerializeField] private GameObject projectilePrefab;
+        
+        public override void DoIt()
+        {
+            Debug.Log("Should Fire!");
+            // GameObject.Instantiate(projectilePrefab, fireOrigin, false);
+        }
+    }
+}
