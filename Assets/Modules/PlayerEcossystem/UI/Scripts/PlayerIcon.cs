8c1974647fd4e16d52df8e328ef50e6fd578d6fb
Marlus Vinicius <marlus.cabral@compasso.com.br>
2024-05-08 22:47:33 -0300

    (update) implement endgame freeze and vitor/lost player icons

--- Assets/Modules/PlayerEcossystem/UI/Scripts/PlayerIcon.cs
+++ Assets/Modules/PlayerEcossystem/UI/Scripts/PlayerIcon.cs
@@ -12,24 +12,40 @@ namespace PlayerEcossystem.UI
         [SerializeField] private Image panel;
         [SerializeField] private Sprite damageSprite;
         [SerializeField] private Sprite victorySprite;
+        [SerializeField] private Sprite loseSprite;
         [SerializeField] private float commandTransitionDuration;
         [SerializeField] private float targetValue = 1f;
         [Min(0)][SerializeField] private int loopAmount = 2;
         [SerializeField] private LoopType loopType;
+        [SerializeField] private Ease flashEase;
+        [SerializeField] private Ease finalEase;
+
+        private Tween flashTween;
 
         private void Start()
         {
-            PlayerManager.OnPlayerDamaged.AddListener(FlashPanels);
-
+            PlayerManager.OnPlayerDamaged.AddListener(FlashDamageSprite);
         }
 
-        public void FlashPanels(int index)
+        public void FlashDamageSprite(int index)
         {
-            Sprite effectSprite = index == playerIndex ? damageSprite : victorySprite;
+            if (playerIndex != index)
+            {
+                return;
+            }
             Sprite originalSprite = panel.sprite;
-            panel.sprite = effectSprite;
-            panel.DOFade(targetValue, commandTransitionDuration)
-                .SetLoops(loopAmount, loopType).OnComplete(() => panel.sprite = originalSprite);
+            panel.sprite = damageSprite;
+            flashTween = panel.DOFade(targetValue, commandTransitionDuration)
+                .SetLoops(loopAmount, loopType)
+                .SetEase(flashEase)
+                .OnComplete(() => panel.sprite = originalSprite);
+        }
+
+        public void PresentFinalSprites(int index)
+        {
+            flashTween.Kill();
+            panel.sprite = index == playerIndex ? victorySprite : loseSprite;
+            flashTween = panel.DOFade(1, 0.5f).SetEase(finalEase);
         }
 
     }
