a529cc2dfa1cb4a90598ac777fe2c7a2ef1aebb0
Marlus Vinicius <marlus.cabral@compasso.com.br>
2024-06-09 14:46:12 -0300

    (fix) soundtrack equalization and (apply) trophy sfx

--- Assets/Modules/Soundtrack/Scripts/SoundtrackManager.cs
+++ Assets/Modules/Soundtrack/Scripts/SoundtrackManager.cs
@@ -7,27 +7,20 @@ namespace Modules.Soundtrack
     public class SoundtrackManager : MonoBehaviour
     {
         public AudioClip duelSoundtrack;
-        public float duelTweenDuration;
-        public float initialVolume;
-        public float gameplayVolume;
 
         private AudioSource source;
 
         private void Start()
         {
             source = GetComponent<AudioSource>();
-            // source.volume = initialVolume;
         }
 
         public void SetDuelSoundtrack()
         {
-            if (source.clip != duelSoundtrack)
-            {
-                source.clip = duelSoundtrack;
-                source.Play();
-            }
-
-            // source.DOFade(gameplayVolume, duelTweenDuration);
+            if (source.clip == duelSoundtrack) 
+                return;
+            source.clip = duelSoundtrack;
+            source.Play();
         }
     }
 }
